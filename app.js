6e0e07f5f96c57f0c6018a80b0e1b372490e2d8c
HannaPleshko <hannapleshko2000@mail.ru>
2023-08-10 16:57:11 +0300

    fix height

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ const squares = document.querySelectorAll('.el');
 let isAnimating = false;
 let isInCenter = false;
 
-// Функция для анимации вращения квадратов
 function animateSquares(rotation) {
     squares.forEach((square, index) => {
 
         square.style.transform = `
         rotate(${rotation * (index + 1)}deg)
         `;
-
     });
 
 }
-
-
 
 let progress = 0;
 
@@ -23,18 +19,13 @@ document.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
     const middleViewport = viewportHeight / 2;
 
-    if (scrollY >= middleViewport && scrollY < viewportHeight) {
-        // squares.style = 'position: fixed'
+    // squares[0].clientHeight // высота квадрата
+
+    if (scrollY >= middleViewport + squares[0].clientHeight && scrollY < viewportHeight) {
         progress = (scrollY - middleViewport) / (viewportHeight / 2);
 
         progress = Math.min(1, Math.max(0, progress));
-
-    } else {
-
-        progress = 0;
-
-    }
-
+    } else progress = 0;
+    
     animateSquares(progress * 360);
-
 });
