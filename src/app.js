892e23b02765842ae73b28fec9c9981c35535323
HannaPleshko <hannapleshko2000@mail.ru>
2023-08-11 15:54:17 +0300

    touch and wheel events

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const squares = document.querySelectorAll('.el');
 let isFrozen = false;
+let progress = 0;
 
 function animateSquares(rotation) {
   squares.forEach((square, index) => {
@@ -7,11 +8,8 @@ function animateSquares(rotation) {
   });
 }
 
-let progress = 0;
-
-document.addEventListener('wheel', () => {
+function mainAction() {
   const viewportHeight = window.innerHeight;
-  const scrollY = window.scrollY;
   const middleViewport = viewportHeight / 2;
 
   let rect = squares[0].getBoundingClientRect();
@@ -25,7 +23,7 @@ document.addEventListener('wheel', () => {
   if (isCenterY) {
     isFrozen = true;
 
-    progress = (scrollY - middleViewport) / (viewportHeight / 2);
+    progress = (centerY - middleViewport) / (viewportHeight / 2);
 
     progress = Math.min(1, Math.max(0, progress));
   } else {
@@ -33,13 +31,52 @@ document.addEventListener('wheel', () => {
     progress = 0;
   }
 
-  if (isFrozen) {
-    // document.body.style.overflow = 'hidden';
-    console.log('frozent', true);
-  } else {
-    // document.body.style.overflow = 'auto';
-    console.log('frozent', false);
-  }
+  // if (isFrozen) {
+  //   console.log('frozent', true);
+  // } else {
+  //   console.log('frozent', false);
+  // }
 
   animateSquares(progress * 360);
+}
+
+let startY;
+let startScrollTop;
+
+document.addEventListener('touchstart', handleTouchStart);
+document.addEventListener('touchmove', handleTouchMove);
+document.addEventListener('wheel', handleWheel);
+
+const body = document.querySelector('.els');
+
+function handleTouchStart(e) {
+  startY = e.touches[0].pageY;
+  startScrollTop = body.scrollTop;
+}
+
+function handleTouchMove(e) {
+  const deltaY = e.touches[0].pageY - startY;
+  body.style.top = `${Number.parseInt(body.style.top || '0') + deltaY}px`;
+  startY = e.touches[0].pageY;
+
+  mainAction();
+}
+
+function handleWheel(e) {
+  body.style.top = `${Number.parseInt(body.style.top || '0') - e.deltaY}px`;
+  startY = e.deltaY;
+
+  mainAction();
+}
+
+document.addEventListener('touchstart', event => {
+  console.log('Вы приложили палец к элементу');
+});
+
+document.addEventListener('touchmove', event => {
+  console.log('По мне ведут пальцем');
+});
+
+document.addEventListener('touchend', event => {
+  console.log('Прикосновение закончено');
 });
